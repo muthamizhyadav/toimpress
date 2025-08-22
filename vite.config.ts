26fbe91e2efbd394cf7831bde27b92afc6bae692
muthamizhyadav <muthamizhyadav@gmail.com>
2025-08-22 12:20:24 +0530

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
   css: {
     postcss: "./postcss.config.js",
   },
   server: {
     host: "0.0.0.0",
     port: 5173,
-    allowedHosts: true, // ✅ allow ALL hosts
+    allowedHosts: true,
   },
 });
