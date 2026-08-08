50513dc531d4c7ea756fce6c8245e601fe3c558c
SelvanSaksh <muthamizh.selvan@sakksh.com>
2026-08-08 22:06:38 +0530

    test: set exchange processing fee to Rs 1

--- src/pages/ExchangeReturn/ExchangeReturnRequests.tsx
+++ src/pages/ExchangeReturn/ExchangeReturnRequests.tsx
@@ -40,7 +40,7 @@ import { loadRazorpay } from "../../utils/loadRazorpay";
 
 const DARK_GREEN = "#133215";
 const LIGHT_GREEN = "#92B775";
-const EXCHANGE_CHARGE = 150;
+const EXCHANGE_CHARGE = 1;
 const RAZORPAY_KEY_ID = import.meta.env.VITE_RZP_KEY_ID as string;
 
 const STATUS_COLORS: Record<string, string> = {
